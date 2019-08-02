0ff06145260101fa8fdd5f5aa0940952faca1167
leetao <798824788@qq.com>
2019-08-02 15:52:49 +0800

    搭建项目

---
+++ ttClinicPlatform/src/main/java/clinicplatform/TtclinicplatformApplication.java
@@ -0,0 +1,13 @@
+package clinicplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtclinicplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtclinicplatformApplication.class, args);
+    }
+
+}
