b3026a92b983174ac6c728f16c95e3c4bdaaa76b
leetao <798824788@qq.com>
2019-08-07 14:02:51 +0800

    iview的zip

--- ttClinicPlatform/src/main/java/clinicplatform/TtclinicplatformApplication.java
+++ ttClinicPlatform/src/main/java/clinicplatform/TtclinicplatformApplication.java
@@ -3,11 +3,9 @@ package clinicplatform;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("clinicplatform.mapper")
-@ComponentScan("clinicplatform.*")  //指定扫描包路径
 public class TtclinicplatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(TtclinicplatformApplication.class, args);
